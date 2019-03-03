f7dbccd5f50ae375dbbef4ce433e4dc848581c61
Thulasi <atfornet@gmail.com>
2019-03-02 19:41:05 -0800

    Updates

---
+++ precache-manifest.41e142acc7c651139087f7d4742d41dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72d2a57eca9de8d97851",
+    "url": "/tic-tac-toe/static/js/runtime~main.72d2a57e.js"
+  },
+  {
+    "revision": "ec66faef495939ba0d8f",
+    "url": "/tic-tac-toe/static/js/main.ec66faef.chunk.js"
+  },
+  {
+    "revision": "ea089c583e222e215502",
+    "url": "/tic-tac-toe/static/js/2.ea089c58.chunk.js"
+  },
+  {
+    "revision": "ec66faef495939ba0d8f",
+    "url": "/tic-tac-toe/static/css/main.82175728.chunk.css"
+  },
+  {
+    "revision": "0376b7e7c537cb04fe78fd404767c265",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
