1fcc2559436812d87b8b14b49e9ba6d4bf566c69
Thulasi <atfornet@gmail.com>
2019-09-26 21:03:49 -0700

    Refactor renderSquares to 2 for loops

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,25 +12,19 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+    let squares = [];
+    for (let i = 0; i < 3; i++) {
+      let row = [];
+      for (let j = 0; j < 3; j++) {
+        row.push(this.renderSquare(i * 3 + j));
+      }
+      squares.push(
+        <div key={i} className="board-row">
+          {row}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      );
+    }
+    return <div> {squares} </div>;
   }
 }
 
